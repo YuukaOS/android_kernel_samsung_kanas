d2cc82d8566641b518b4fb1c3d8f6808b830ee8e
Junjie Wu <junjiew@codeaurora.org>
2014-01-23 11:15:22 -0800

    cpufreq: Set policy for NULL before cleaning up kobj
    __cpufreq_remove_dev_finish() cleans up the policy->kobj while
    per_cpu(cpufreq_cpu_data) still contains a valid policy pointer.
    This causes a race between kobject_get() called from cpufreq_cpu_get()
    and cpu hotplug.
    
    Set cpufreq_cpu_data to NULL before cleaning up kobject so that
    subsequent cpufreq_cpu_get() will fail and thus not race with
    cpu hotplug.
    
    Change-Id: I0e2d1a64b7aac98aa69e137cc902e07d0edb786e
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1265,6 +1265,7 @@ static int __cpufreq_remove_dev_finish(struct device *dev,
 
 	read_lock_irqsave(&cpufreq_driver_lock, flags);
 	policy = per_cpu(cpufreq_cpu_data, cpu);
+	per_cpu(cpufreq_cpu_data, cpu) = NULL;
 	read_unlock_irqrestore(&cpufreq_driver_lock, flags);
 
 	if (!policy) {
@@ -1334,7 +1335,6 @@ static int __cpufreq_remove_dev_finish(struct device *dev,
 		}
 	}
 
-	per_cpu(cpufreq_cpu_data, cpu) = NULL;
 	return 0;
 }
 
