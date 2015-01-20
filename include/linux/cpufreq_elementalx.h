e54f4238cf7bee4b4f78b5c689b4934b759b21f6
flar2 <asegaert@gmail.com>
2015-01-19 23:24:26 -0500

    ElementalX governor version 2
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

---
+++ include/linux/cpufreq_elementalx.h
@@ -0,0 +1,13 @@
+/*
+ * linux/include/linux/cpufreq_elementalx.h
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#ifndef _LINUX_ELEMENTALX_H
+#define _LINUX_ELEMENTALX_H
+
+extern int graphics_boost;
+
+#endif /* _LINUX_ELEMENTALX_H */
