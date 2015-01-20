3d20d603597bfd18d87de61d2f244181a3b5df04
flar2 <asegaert@gmail.com>
2015-01-19 20:19:28 -0500

    Add cpufreq_notify_utilization
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -117,6 +117,8 @@ struct cpufreq_policy {
 
 	struct kobject		kobj;
 	struct completion	kobj_unregister;
+
+	unsigned int util;
 };
 
 #define CPUFREQ_ADJUST			(0)
@@ -144,6 +146,7 @@ static inline bool policy_is_shared(struct cpufreq_policy *policy)
 #define CPUFREQ_POSTCHANGE	(1)
 #define CPUFREQ_RESUMECHANGE	(8)
 #define CPUFREQ_SUSPENDCHANGE	(9)
+unsigned int cpufreq_quick_get_util(unsigned int cpu);
 
 struct cpufreq_freqs {
 	unsigned int cpu;	/* cpu nr */
@@ -285,6 +288,8 @@ int cpufreq_unregister_driver(struct cpufreq_driver *driver_data);
 
 void cpufreq_notify_transition(struct cpufreq_policy *policy,
 		struct cpufreq_freqs *freqs, unsigned int state);
+void cpufreq_notify_utilization(struct cpufreq_policy *policy,
+		unsigned int load);
 
 static inline void cpufreq_verify_within_limits(struct cpufreq_policy *policy, unsigned int min, unsigned int max)
 {
