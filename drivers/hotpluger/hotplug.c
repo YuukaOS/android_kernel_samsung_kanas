90ee0468c8bf6a40c7d7c0770caa6d7d0e7332f1
yui <>
2017-06-16 16:58:32 +0800

    hotplugger: major refractoring
    1.Check for name and null pointers in register/unregister functions
    2.Find drivers with pointers and "name"(char *) to detect duplicated
    entries in register/unregister functions
    3.Lock mutexes before checking whether the switch/enable variable is true, or
    accessing the lists read or write.
    4.Be more verbose, add more pr_debug functions
    5.Code beautification: limit to 80 chars per line, space indent line broken
    arguments, a space between () and {, remove most of the trailing whitespace,
    add more new line between parts of code in a function, change " , " to just
    ", ".
    6.Remove num_drivers_loaded variable as we don't actually use it
    7.Add a pr_debug when the buffer (for sysfs *show* funcs) is reached
    8.Re-structure code to prevent labels and goto; use if-else statements
    9.Add module description

--- drivers/hotpluger/hotplug.c
+++ drivers/hotpluger/hotplug.c
@@ -20,14 +20,13 @@
 
 #include <linux/hotplugger.h>
 
-static int num_drivers_loaded = 0;
 static LIST_HEAD(hotplugger_driver_list);
 static DEFINE_MUTEX(hotplugger_driver_mutex);
 atomic_t enabled = ATOMIC_INIT(1);
 
 static struct hotplugger_driver *__find_driver(const char *name)
 {
-	struct hotplugger_driver *d;
+	struct hotplugger_driver *d = NULL;
 	int ret;
 #ifdef DEBUG
 	int i = 0;
@@ -35,13 +34,44 @@ static struct hotplugger_driver *__find_driver(const char *name)
 
 	pr_debug("%s was called\n", __func__);
 
-	list_for_each_entry(d , &hotplugger_driver_list, list) {
+	if (name == NULL) {
+		pr_debug("%s: driver name search query is NULL\n", __func__);
+		return NULL;
+	}
+
+	list_for_each_entry(d, &hotplugger_driver_list, list) {
 		if (!(ret = strnicmp(name, d->name, DRIVER_NAME_LEN)))
 			return d;
 #ifdef DEBUG
 		pr_debug("%s: i is %d\n", __func__, ++i);
-		pr_debug("%s: name is %s, driver is %s\n", __func__, name , d->name);
+		pr_debug("%s: name is %s, driver is %s\n", __func__, name, d->name);
 		pr_debug("%s: result is %d\n",  __func__, ret);
+#endif
+	}
+
+	return NULL;
+}
+
+static struct hotplugger_driver *__find_driver_ptr(struct hotplugger_driver *driver)
+{
+	struct hotplugger_driver *d = NULL;
+#ifdef DEBUG
+	int i = 0;
+#endif
+
+	pr_debug("%s was called\n", __func__);
+
+	if (driver == NULL) {
+		pr_debug("%s: driver var is NULL\n", __func__);
+		return NULL;
+	}
+
+	list_for_each_entry(d, &hotplugger_driver_list, list) {
+		if (driver == d)
+			return d;
+#ifdef DEBUG
+		pr_debug("%s: i is %d\n", __func__, ++i);
+		pr_debug("%s: driver at %p, d at %p\n", __func__, driver, d);
 #endif
 	}
 
@@ -63,6 +93,10 @@ static bool __find_name_in_list(char **list, char *name)
 		pr_debug("%s: list is EMPTY\n", __func__);
 		return false;
 	}
+	if (name == NULL) {
+		pr_debug("%s: driver name is NULL\n", __func__);
+		return false;
+	}
 
 #ifdef DEBUG
 	pr_debug("%s: ----------list start----------\n", __func__);
@@ -75,7 +109,7 @@ static bool __find_name_in_list(char **list, char *name)
 
 	for ( i = 0 ; (l = *list) ; i++, list++) {
 		pr_debug("%s: i is %d\n", __func__, i);
-		pr_debug("%s: name is %s, list[%d] is %s\n", __func__, name , i, l);
+		pr_debug("%s: name is %s, list[%d] is %s\n", __func__, name, i, l);
 		if (!strnicmp(l, name, DRIVER_NAME_LEN)) {
 			pr_debug("%s: result is TRUE\n", __func__);
 			return true;
@@ -91,7 +125,7 @@ static inline int __state_change(struct hotplugger_driver *caller,
                                  struct hotplugger_driver *d,
                                  bool state)
 {
-	int ret = 0;
+	int ret = -EFAULT;
 	
 	pr_debug("%s was called\n", __func__);
 
@@ -106,15 +140,16 @@ static inline int __state_change(struct hotplugger_driver *caller,
 			pr_debug("%s: %s: %pf failed with err %d\n", __func__,
 		         d->name, d->change_state, ret);
 		}
-	} else if (d->is_enabled() == state) {
+	}
+#ifdef DEBUG
+	  else if (d->is_enabled() == state) {
 		pr_debug("%s: \"%s\" driver is already %s\n", __func__,
 		         d->name, state ? "enabled" : "disabled");
-		ret = -EFAULT;
 	} else if (!d->change_state) {
 		pr_debug("%s: change_state for \"%s\" driver is NULL\n", __func__,
 		         d->name);
-		ret = -EFAULT;
 	}
+#endif
 
 	return ret;
 }
@@ -136,22 +171,35 @@ static inline ssize_t __show_drivers_by_state(char *buf, bool state)
 
 	pr_debug("%s was called\n", __func__);
 
-	if (!num_drivers_loaded) {
-		i += sprintf(buf, "NaN");
-		goto out;
-	}
+	mutex_lock(&hotplugger_driver_mutex);
 
 	list_for_each_entry(d, &hotplugger_driver_list, list) {
+		if (i >= buf_size) {
+			pr_debug("%s: buffer is full...\n", __func__);
+			break;
+		}
+
 		pr_debug("%s: j is %d\n", __func__, ++j);
-		if (i >= buf_size)
-			goto out;
-		if (d->is_enabled() == state) {
+		if (d && d->name && d->is_enabled && d->is_enabled() == state) {
 			pr_debug("%s: i was %d\n", __func__, i);
 			i += scnprintf(&buf[i], DRIVER_NAME_LEN + 1, "%s ", d->name);
 			pr_debug("%s: i is now %d\n", __func__, i);
+		} 
+#ifdef DEBUG
+		  else if (!d->name) {
+			pr_debug("%s: d->name is NULL\n", __func__);
+		} else if (!d->is_enabled) {
+			pr_debug("%s: d->is_enabled is NULL\n", __func__);
+		} else if (!d) {
+			pr_debug("%s: d is NULL\n", __func__);
 		}
+#endif
 	}
-out:
+	if (i == 0)
+		i += sprintf(buf, "NaN");
+
+	mutex_unlock(&hotplugger_driver_mutex);
+
 	i += sprintf(&buf[i], "\n");
 	pr_debug("%s: printed %d bytes\n", __func__, i);
 	return i;
@@ -166,7 +214,7 @@ static inline ssize_t __store_state_by_name(const char *buf,
 
 	pr_debug("%s was called\n", __func__);
 
-	if (atomic_read(&enabled) <= 0){
+	if (atomic_read(&enabled) <= 0) {
 		pr_debug("%s: hotplugger is disabled\n", __func__);
 		return -EPERM;
 	}
@@ -179,7 +227,7 @@ static inline ssize_t __store_state_by_name(const char *buf,
 
 	new = __find_driver(name);
 
-	if (new == NULL){
+	if (new == NULL) {
 		pr_debug("%s: \"%s\" driver is not found\n", __func__, name);
 		return -EINVAL;
 	} else {
@@ -195,7 +243,9 @@ static inline ssize_t __store_state_by_name(const char *buf,
 		return count;
 }
 
-/*********************************sysfs start*********************************/
+/*************
+ * sysfs start
+ *************/
 
 static ssize_t show_enabled(struct device *dev,
                             struct device_attribute *attr, char *buf)
@@ -227,9 +277,8 @@ static ssize_t store_enabled(struct device *dev,
 	}
 
 	state = input > 0 ? true : false;
-	pr_debug("%s: %s functions\n", __func__, 
-	          state ? "enabling" : "disabling");
-	pr_debug("%s: setting %s state\n", __func__,
+	pr_debug("%s: setting %s state\n",
+	          __func__,
 	          state ? "enabled" : "disabled");
 	atomic_set(&enabled, state);
 
@@ -275,29 +324,35 @@ static ssize_t show_available_drivers(struct device *dev,
                                       char *buf)
 {
 	struct hotplugger_driver *d;
+#ifdef DEBUG
 	int j = 0;
+#endif
 	ssize_t i = 0;
 	ssize_t buf_size = ((PAGE_SIZE / sizeof(char)) - (DRIVER_NAME_LEN + 2));
 
 	pr_debug("%s was called\n", __func__);
 
-	if (!num_drivers_loaded) {
-		i += sprintf(buf, "NaN");
-		goto out;
-	}
+	mutex_lock(&hotplugger_driver_mutex);
 
 	list_for_each_entry(d, &hotplugger_driver_list, list) {
-		pr_debug("%s: j is %d\n", __func__, ++j);
-		if (i >= buf_size)
-			goto out;
-		pr_debug("%s: i was %d\n", __func__, i);
+		if (i >= buf_size) {
+			pr_debug("%s: buffer is full...\n", __func__);
+			break;
+		}
+		pr_debug("%s: j is %d\n"
+		         "%s: i was %d\n",
+		          __func__, ++j, __func__, i);
 		if (d->is_enabled())
 			i += scnprintf(&buf[i], DRIVER_NAME_LEN + 1, "[%s] ", d->name);
 		else
 			i += scnprintf(&buf[i], DRIVER_NAME_LEN + 1, "%s ", d->name);
 		pr_debug("%s: i is now %d\n", __func__, i);
 	}
-out:
+	if (i == 0)
+		sprintf(buf, "NaN");
+
+	mutex_unlock(&hotplugger_driver_mutex);
+
 	i += sprintf(&buf[i], "\n");
 	pr_debug("%s: finishing with %d bytes\n", __func__, i);
 	return i;
@@ -321,41 +376,73 @@ static struct attribute_group hotplugger_attr_group = {
 	.name = "hotplugger",
 };
 
-/*********************************sysfs end*********************************/
+/*************
+ * sysfs end
+ *************/
 
 int hotplugger_register_driver(struct hotplugger_driver *driver)
 {
 	int err = -EINVAL;
+	struct hotplugger_driver *d;
 
-	if (!driver)
+	pr_debug("%s was called\n", __func__);
+
+	if (!driver) {
+		pr_debug("%s: driver is NULL\n", __func__);
 		return err;
+	}
+
+	if (!driver->name) {
+		pr_debug("%s: driver name is NULL\n", __func__);
+		return err;
+	}
 
 	mutex_lock(&hotplugger_driver_mutex);
-	err = -EBUSY;
-	if (__find_driver(driver->name) == NULL) {
-		err = 0;
-		num_drivers_loaded++;
+	/* Checks */
+	d = __find_driver(driver->name);
+	if (d && (d != driver)) {
+		pr_debug("%s: A driver with name \"%s\" exists!\n",
+		          __func__, driver->name);
+	} else if (d && d == driver) {
+		pr_debug("%s: driver \"%s\" already registered\n",
+		          __func__, driver->name);
+	} else {
 		list_add(&driver->list, &hotplugger_driver_list);
+		err = 0;
 	}
- 
 	mutex_unlock(&hotplugger_driver_mutex);
+
 	return err;
 }
 EXPORT_SYMBOL_GPL(hotplugger_register_driver);
 
 void hotplugger_unregister_driver(struct hotplugger_driver *driver)
 {
-	if (!driver)
-		return;
+	struct hotplugger_driver *d;
 
-	if (__find_driver(driver->name) == NULL) {
-		if (driver->is_enabled())
-			driver->change_state(false);
+	pr_debug("%s was called\n", __func__);
+
+	if (!driver) {
+		pr_debug("%s: driver is NULL\n", __func__);
+		return;
 	}
 
 	mutex_lock(&hotplugger_driver_mutex);
-	list_del(&driver->list);
-	num_drivers_loaded--;
+	d = __find_driver(driver->name);
+	if ((d) && (d == driver)) {
+			__state_change(NULL, driver, false);
+			pr_debug("%s: Removing \"%s\" driver from list\n",
+			          __func__, driver->name);
+			list_del(&driver->list);
+	} else if ((d) && (d != driver)) {
+		pr_debug("%s: A driver with name \"%s\" exists but their pointers"
+		         "differ [%p(list) =/= %p(yours)]\n",
+		         __func__, driver->name, d, driver);
+	} else {
+		pr_debug("%s: No matching \"%s\" driver found!\n",
+		         __func__, driver->name);
+	}
+
 	mutex_unlock(&hotplugger_driver_mutex);
 	return;
 }
@@ -378,6 +465,7 @@ static int __init hotplugger_init(void)
 int hotplugger_disable_conflicts(struct hotplugger_driver *driver)
 {
 	struct hotplugger_driver *d;
+	int ret = 0;
 
 	pr_debug("%s was called\n", __func__);
 
@@ -387,82 +475,77 @@ int hotplugger_disable_conflicts(struct hotplugger_driver *driver)
 		return -ENXIO;
 	}
 
-	if (mutex_is_locked(&hotplugger_driver_mutex)){
+	if (mutex_is_locked(&hotplugger_driver_mutex)) {
 		pr_debug("%s: another \"disabling\" is in progress\n", __func__);
 		return -EBUSY;
 	}
 
-	if (atomic_read(&enabled) <= 0){
+	mutex_lock(&hotplugger_driver_mutex);
+
+	if (atomic_read(&enabled) <= 0) {
         pr_debug("%s: enabled = %d \n", __func__, atomic_read(&enabled));
 		pr_debug("%s: hotplugger is disabled\n", __func__);
 		return -EPERM;
 	}
 
-	mutex_lock(&hotplugger_driver_mutex);
-
 	/* TODO: refactor this whole function as it uses more than 3 linear searches
 	 *       and a lot of duplications due to design error.
-	 *       No one would try and register about 10K hotplug drivers/modules
+	 *       No one would try and register about 100 hotplug drivers/modules
 	 *       right? */
-	/* First check-if driver exists */
-	list_for_each_entry(d, &hotplugger_driver_list, list) {
-		if (d == driver)
-			break;
-	}
+	/* First, check if driver exists */
+	d =__find_driver_ptr(driver);
 	if (d == NULL) {
 		pr_debug("%s: driver \"%s\" is unregistered\n", __func__, driver->name);
 		pr_debug("%s: aborting...\n", __func__);
-		return -ENODEV;
-	}
+		ret = -ENODEV;
+	} else {
+		pr_debug("%s: driver \"%s\" requests conflict resolution\n", __func__,
+		          driver->name);
 
-	pr_debug("%s: driver \"%s\" requests conflict resolution\n", __func__,
-		   driver->name);
-
-	/* Check if the driver specified a white list*/
-	if (driver->whitelist && *driver->whitelist != NULL){
-		pr_debug("%s: whitelist found!\n", __func__);
-		pr_debug("%s: whitelist address: %p \n", __func__, driver->whitelist);
-		pr_debug("%s: whitelist first: %p \n", __func__, *driver->whitelist);
-		pr_debug("%s: first string is \"%s\"!\n", __func__, *driver->whitelist);
-		list_for_each_entry(d , &hotplugger_driver_list, list) {
-			/* check if the other driver's name is on the whitelist */
-			if (!d->is_enabled())
-				continue;
-
-			if (d != driver &&
-				  __find_name_in_list(driver->whitelist, d->name) == true) {
-				pr_debug("%s: driver \"%s\" is whitelisted in  driver \"%s\".\n",
-				         __func__, d->name, driver->name);
-
-				/* We need both of the drivers to whitelist themselves as a 
-				   safety measure */
-				if (d->whitelist && *d->whitelist == NULL) {
-				pr_debug("%s: BUT driver \"%s\" is NOT whitelisted"
-				         " in  driver \"%s\".\n",
-				         __func__, driver->name, d->name);
-					hotplugger_disable_one(driver, d);
-				} else if ((d->whitelist && *d->whitelist != NULL) &&
-					__find_name_in_list(d->whitelist, driver->name) == true) {
-					pr_debug("%s: Both are whitelisted for each other.\n",
-							__func__);
+		/* Check if the driver specified a white list*/
+		if (driver->whitelist && *driver->whitelist != NULL) {
+			pr_debug("%s: whitelist found!\n", __func__);
+			pr_debug("%s: list address: %p \n", __func__, driver->whitelist);
+			pr_debug("%s: first element: %p \n", __func__, *driver->whitelist);
+			list_for_each_entry(d, &hotplugger_driver_list, list) {
+				/* check if the other driver's name is on the whitelist */
+				if (!d->is_enabled())
 					continue;
+
+				if (d != driver &&
+					__find_name_in_list(driver->whitelist, d->name) == true) {
+					pr_debug("%s: driver \"%s\" is whitelisted in  driver \"%s\".\n",
+					          __func__, d->name, driver->name);
+
+					/* We need both of the drivers to whitelist themselves as a
+					   safety measure
+					 */
+					if (d->whitelist && *d->whitelist == NULL) {
+						pr_debug("%s: BUT driver \"%s\" is NOT whitelisted"
+						         " in  driver \"%s\".\n",
+						          __func__, driver->name, d->name);
+						hotplugger_disable_one(driver, d);
+					} else if ((d->whitelist && *d->whitelist != NULL) &&
+						__find_name_in_list(d->whitelist, driver->name) == true) {
+						pr_debug("%s: Both are whitelisted for each other.\n",
+						          __func__);
+						continue;
+					}
+				} else {
+					hotplugger_disable_one(driver, d);
 				}
-			} else {
-				hotplugger_disable_one(driver, d);
 			}
-		}
-    } else if ((driver->whitelist == NULL) ||
-		       (driver->whitelist && *driver->whitelist == NULL))
-	{
-		pr_debug("%s: driver \"%s\" has empty lists, disabling all\n",
-			   __func__, driver->name);
-		list_for_each_entry(d , &hotplugger_driver_list, list) {
-			hotplugger_disable_one(driver, d);
+		} else if ((driver->whitelist == NULL) ||
+		           (driver->whitelist && *driver->whitelist == NULL)) {
+			pr_debug("%s: driver \"%s\" has empty lists, disabling all\n",
+			          __func__, driver->name);
+			list_for_each_entry(d, &hotplugger_driver_list, list)
+				hotplugger_disable_one(driver, d);
 		}
 	}
 
 	mutex_unlock(&hotplugger_driver_mutex);
-	return 0;
+	return ret;
 }
 EXPORT_SYMBOL_GPL(hotplugger_disable_conflicts);
 
@@ -473,14 +556,15 @@ int hotplugger_enable_one(const char *name)
 	pr_debug("%s was called with \"%s\" as its first parameter\n",
 	         __func__, name);
 
-	if (atomic_read(&enabled) <= 0){
+	if (atomic_read(&enabled) <= 0) {
         pr_debug("%s: enabled = %d \n", __func__, atomic_read(&enabled));
 		pr_debug("%s: hotplugger is disabled\n", __func__);
 		return -EPERM;
 	}
 
 	if (d == NULL) {
-		pr_debug("%s: \"%s\" driver cannot be found\n", __func__, name);
+		pr_debug("%s: \"%s\" driver cannot be found\n",
+		          __func__, name);
 		return -EINVAL;
 	}
 
@@ -491,7 +575,8 @@ int hotplugger_enable_one(const char *name)
 EXPORT_SYMBOL_GPL(hotplugger_enable_one);
 
 MODULE_AUTHOR("ME");
-
+MODULE_DESCRIPTION("Manage hotplug modules so they won't run simultaneously"
+                   "naivete style");
 MODULE_LICENSE("GPL");
 
 module_init(hotplugger_init);
