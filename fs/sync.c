9758ea226db856a44a06b993f3d7b25fd77927b2
Mark Enriquez <enriquezmark36@gmail.com>
2017-07-01 19:08:57 +0800

    fs/dyn_sync_cntrl: dynamic sync control
    The dynamic sync control interface uses new power_suspend interface
    
    While screen is on, file sync is disabled
    when screen is off, a file sync is called to flush all outstanding writes
    and restore file sync operation as normal.
    
    updated for 8974 kernels
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Actually this is converted back to early_suspend api so it can be used
    with drivers with early_suspend implemented without doing a nation-wide
    port.
    
    (cherry picked from commit ecd6f7a6f9822431a8f30794abcc3aad9b1a66c5)
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -18,7 +18,7 @@
 #include "internal.h"
 
 #ifdef CONFIG_DYNAMIC_FSYNC
-extern bool power_suspend_active;
+extern bool early_suspend_active;
 extern bool dyn_fsync_active;
 #endif
 
@@ -94,12 +94,13 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 	filemap_fdatawait(bdev->bd_inode->i_mapping);
 }
 
-
-#ifdef CONFIG_DYNAMIC_FSYNC
 /*
  * Sync all the data for all the filesystems (called by sys_sync() and
  * emergency sync)
  */
+#ifndef CONFIG_DYNAMIC_FSYNC
+static
+#endif
 void sync_filesystems(int wait)
 {
 	iterate_supers(sync_inodes_one_sb, NULL);
@@ -108,6 +109,8 @@ void sync_filesystems(int wait)
 	iterate_bdevs(fdatawrite_one_bdev, NULL);
 	iterate_bdevs(fdatawait_one_bdev, NULL);
 }
+#ifdef CONFIG_DYNAMIC_FSYNC
+EXPORT_SYMBOL_GPL(sync_filesystems);
 #endif
 
 /*
@@ -199,7 +202,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -242,7 +245,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -251,6 +254,11 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+#if 0
+	if (likely(dyn_fsync_active && !power_suspend_active))
+		return 0;
+	else
+#endif
 	return do_fsync(fd, 1);
 }
 
@@ -322,7 +330,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 				unsigned int, flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -415,7 +423,7 @@ SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && !power_suspend_active))
+	if (likely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
