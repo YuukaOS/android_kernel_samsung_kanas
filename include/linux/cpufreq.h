521e7c8237440886d7a621b608f89a7e3a2ce83b
Mark Enriquez <enriquezmark36@gmail.com>
2017-07-30 19:22:48 +0800

    cpufreq_lionfish: Activate
    cpufreq_lionfish.c is already in the source but the corresponding Kconfig and Makefile entries
    are not. The kernel is okay with switching governors. For what ever reason the guy who did this
    may have a good reason why not to include this.
    
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -420,6 +420,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
+extern struct cpufreq_governor cpufreq_gov_lionfish;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lionfish)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
